d8d5ea43e7507057c958678d6db52bac33458c2b
thepritampatil <thepritampatil@gmail.com>
2026-07-12 15:17:17 +0530

    new update

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const age =24;
-const name ='Pritam Patil'
-const dob ='23/01/2003'
+[1mdiff --git a/index.js b/index.js[m
+[1mindex 716002f..196fc47 100644[m
+[1m--- a/index.js[m
+[1m+++ b/index.js[m
+[36m@@ -3,5 +3,9 @@[m [mconst name ='Pritam Patil'[m
+ const dob ='23/01/2003'[m
+ [m
+ function sayName(){[m
+[31m-    console.log('my name'+ name)[m
+[32m+[m[32m    console.log('my name is :'+ name)[m
+[32m+[m[32m}[m
+[32m+[m
+[32m+[m[32mfunction isAbove18(){[m
+[32m+[m[32m    return age>=18[m
+ }[m
+\ No newline at end of file[m
+    
 
-function sayName(){
-    console.log('my name'+ name)
-}
